4a9043fd88281a461dfdd296d0157209a3ddd3b3
delandapp <alihandronikolas@gmail.com>
2024-04-25 13:38:11 +0700

    'update'

--- lib/app/data/constans/endpoint.dart
+++ lib/app/data/constans/endpoint.dart
@@ -1,5 +1,5 @@
 class Endpoint {
-  static const String baseUrlApi = "http://192.168.1.6:8000/api/users/";
+  static const String baseUrlApi = "http://192.168.88.190:8000/api/users/";
 
    static const String profil = "${baseUrlApi}profil";
   static const String updateProfile = "${baseUrlApi}update";
